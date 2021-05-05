c6356a5313cc71e49eebf5dfdcf2609e3df2ca99
netimiz <mustafa.cakmak@outlook.com>
2021-05-05 21:52:56 +0300

    v0.1

---
+++ panel/application/views/includes/include_style.php
@@ -0,0 +1,6 @@
+<!-- CSS files -->
+<link href="<?php echo base_url("assets/ui_assets"); ?>/dist/css/tabler.min.css" rel="stylesheet"/>
+<link href="<?php echo base_url("assets/ui_assets"); ?>/dist/css/tabler-flags.min.css" rel="stylesheet"/>
+<link href="<?php echo base_url("assets/ui_assets"); ?>/dist/css/tabler-payments.min.css" rel="stylesheet"/>
+<link href="<?php echo base_url("assets/ui_assets"); ?>/dist/css/tabler-vendors.min.css" rel="stylesheet"/>
+<link href="<?php echo base_url("assets/ui_assets"); ?>/dist/css/demo.min.css" rel="stylesheet"/>
